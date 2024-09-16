7bb1ca99519bb5ecc65b2dae3c7f73b231813dea
jashwantbadiya <bscs2012295@szabist.pk>
2024-09-16 12:49:34 +0500

    commit

--- dotnet-hello-world/hello-world-api/Controllers/HelloController.cs
+++ dotnet-hello-world/hello-world-api/Controllers/HelloController.cs
@@ -13,7 +13,7 @@ namespace hello_world_api.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Hello world jackie smith 9001";
+            return "Hello world jackie smith 9002";
         }
 
     }
